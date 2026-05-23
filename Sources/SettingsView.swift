d07e1fc5af25bd908b3e4dea2176cc84eb2543bb
OleksiyM <oleksiymaliovanyy@gmail.com>
2026-05-23 18:41:46 +0300

    ux: replace Form with ScrollView in SettingsView and reduce window height to prevent bottom clipping

--- Sources/SettingsView.swift
+++ Sources/SettingsView.swift
@@ -4,7 +4,7 @@ struct SettingsView: View {
     @ObservedObject var appState: AppState
     
     var body: some View {
-        Form {
+        ScrollView {
             VStack(alignment: .leading, spacing: 20) {
                 // Header
                 HStack {
@@ -209,8 +209,8 @@ struct SettingsView: View {
                     .padding(.vertical, 2)
                 }
             }
-            .padding()
+            .padding(24)
         }
-        .frame(width: 480, height: 740)
+        .frame(width: 480, height: 580)
     }
 }
