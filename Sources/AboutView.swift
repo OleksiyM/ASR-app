dfeb7a652a73fc8db9cb3ab3b672df8dbe03d62b
OleksiyM <oleksiymaliovanyy@gmail.com>
2026-05-23 13:38:41 +0300

    chore: bump app version to 1.2.2

--- Sources/AboutView.swift
+++ Sources/AboutView.swift
@@ -50,7 +50,7 @@ struct AboutView: View {
                         .font(.system(size: 24, weight: .bold))
                         .foregroundColor(.primary)
                     
-                    Text("\(appState.localizedString("version")) 1.2.1")
+                    Text("\(appState.localizedString("version")) 1.2.2")
                         .font(.system(size: 11, weight: .semibold, design: .monospaced))
                         .foregroundColor(.secondary)
                         .padding(.vertical, 2)
